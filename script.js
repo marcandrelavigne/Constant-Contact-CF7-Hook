a40347afc1c62f6b8924facce7aeeb651c3b65b2
Marc-André Lavigne <marcandre.lavigne@me.com>
2019-11-11 15:40:34 -0500

    Formating

--- script.js
+++ script.js
@@ -12,23 +12,23 @@
 		}
 	})();
 	
-  // Ajax Hook
+	// Ajax Hook
 	function newsletterHook() {
 		document.addEventListener( 'wpcf7mailsent', function( event ) {
 			$.ajax({
 				url: "/wp/wp-admin/admin-ajax.php",
 				type: 'POST',
-			    data: {
-				    firstname: $('#js-newsletter input[name="first-name"]').val(),
-				    lastname: $('#js-newsletter input[name="last-name"]').val(),
-				    email: $('#js-newsletter input[name="email"]').val(), 
-				    company: $('#js-newsletter input[name="company"]').val(), 
-				    action: 'newsletter_ajax'
+			    	data: {
+					firstname: $('#js-newsletter input[name="first-name"]').val(),
+					lastname: $('#js-newsletter input[name="last-name"]').val(),
+					email: $('#js-newsletter input[name="email"]').val(), 
+					company: $('#js-newsletter input[name="company"]').val(), 
+					action: 'newsletter_ajax'
 				},
-			  success: function(result){
-          var obj = result.toString();
+			  	success: function(result){
+          				var obj = result.toString();
 					console.log(obj);
-		    }
+		    		}
 			})
 		}, false );
 	}
